14bc48c2af4cce8df66fe24daab072887fa7320f
sahilsajid6-eng <sahilsajid6@gmail.com>
2026-09-19 19:13:59 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def main_dashboard():
     expenses = db_mgr.fetch_user_expenses(user.id)
     curr = demo.currency_symbol
 
-    st.title("💸 Executive Dashboard")
+    st.title("💸 Dashboard")
     st.caption("Track, record, and manage personal expenses effortlessly.")
 
     # High-level Metrics Row
